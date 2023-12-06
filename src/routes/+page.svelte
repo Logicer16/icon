18599e5e2cbcd595b928deda80af72bfbd5f19db
Logicer <logicer@logicer.cc>
2023-12-06 22:20:11 +1100

    add background colour picker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,12 @@
   import {derived, writable, type Writable} from "svelte/store";
   import {processSvg, type SVGData} from "$lib/svgManipulator/svgManipulator";
   import {themeColour, title} from "$lib/const";
-  import {contrastCSSColor} from "$lib/contrast";
+  import ColourPicker from "$lib/ColourPicker/ColourPicker.svelte";
   import Download from "$lib/Download/Download.svelte";
   import SvgPreview from "$lib/SVGPreview/SVGPreview.svelte";
 
-  let fillColour: Writable<string> | undefined = writable(themeColour);
+  let fillColour: Writable<string> = writable(themeColour);
+  let backgroundColour: Writable<string> | undefined = writable(themeColour);
 
   let svg = derived(
     [fillColour],
@@ -14,8 +15,6 @@
       set(processSvg({fillColour: $fillColour}));
     }
   );
-
-  let textColor = contrastCSSColor(fillColour);
 </script>
 
 <div class="container mx-auto space-y-8 p-8">
@@ -23,23 +22,11 @@
   <div class="w-80">
     <SvgPreview svg="{$svg}"></SvgPreview>
   </div>
-  {#if $fillColour !== ""}
-    <div class="inline-flex space-x-2 place-self-center">
-      <div
-        class=" rounded-full px-5 py-[9px]"
-        style="background-color: {$fillColour}; color: {$textColor}">
-        Fill
-      </div>
-      <!-- This will throw an error regarding the format of the hex colour on page load. This has no real effect as it cannot occur. -->
-      <!-- border-solid is applied in app.postcss -->
-      <input
-        class="input border-4 border-solid"
-        type="color"
-        name="fill"
-        id="color-picker-fill"
-        style="border-color: {$textColor}; transition: border-color 0s;"
-        bind:value="{$fillColour}" />
-    </div>
-  {/if}
+  <ColourPicker bind:colour="{fillColour}" id="fill" displayName="Fill"
+  ></ColourPicker>
+  <ColourPicker
+    bind:colour="{backgroundColour}"
+    id="background"
+    displayName="Background"></ColourPicker>
   <Download svg="{$svg}"></Download>
 </div>
